aea55eefeef8181588469615a8bdbf0df030ea52
ZonePG <619586533@qq.com>
2021-10-09 15:17:05 +0800

    add string/151-reverse-words-in-a-string/

---
+++ string/151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  string reverseWords(string s) {
+    reverse(s.begin(), s.end());
+    int idx = 0;
+    for (int start = 0; start < s.size(); start++) {
+      if (s[start] != ' ') {
+        if (idx != 0) {
+          s[idx++] = ' ';
+        }
+        int end = start;
+        while (end < s.size() && s[end] != ' ') {
+          s[idx++] = s[end++];
+        }
+        reverse(s.begin() + idx - (end - start), s.begin() + idx);
+        start = end;
+      }
+    }
+    s.erase(s.begin() + idx, s.end());
+    return s;
+  }
+};
