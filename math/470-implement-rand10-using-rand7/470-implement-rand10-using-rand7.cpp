d8bab61b134c0290a67dfab9f311811e6ff7f681
ZonePG <zonepg666@gmail.com>
2022-01-02 18:33:02 +0800

    add math/470-implement-rand10-using-rand

---
+++ math/470-implement-rand10-using-rand7/470-implement-rand10-using-rand7.cpp
@@ -0,0 +1,15 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+  int rand10() {
+    while (true) {
+      int n = (rand7() - 1) * 7 + rand7();
+      if (n <= 40) {
+        return n % 10 + 1;
+      }
+    }
+  }
+};
