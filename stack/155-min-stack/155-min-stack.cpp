a0d29e25611755ff3111bb7bf343b659ce327c7b
ZonePG <zonepg666@gmail.com>
2022-01-02 18:15:11 +0800

    add stack/155-min-stack/

---
+++ stack/155-min-stack/155-min-stack.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <climits>
+#include <stack>
+
+using namespace std;
+
+class MinStack {
+  stack<int> stk;
+  stack<int> min_stk;
+public:
+  MinStack() {
+    min_stk.push(INT_MAX);
+  }
+
+  void push(int val) {
+    stk.push(val);
+    if (val <= min_stk.top()) {
+      min_stk.push(val);
+    }
+  }
+
+  void pop() {
+    if (stk.top() <= min_stk.top()) {
+      min_stk.pop();
+    }
+    stk.pop();
+  }
+
+  int top() {
+    return stk.top();
+  }
+
+  int getMin() {
+    return min_stk.top();
+  }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
